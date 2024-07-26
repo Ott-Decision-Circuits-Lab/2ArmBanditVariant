f5c9cf393abf0128cbd23f627618a1abc494a1bb
OttLabIT <116281282+OttLabIT@users.noreply.github.com>
2024-07-26 13:09:31 +0200

    Try catch to prevent pharmacology info error

--- TwoArmBanditVariant.m
+++ TwoArmBanditVariant.m
@@ -65,14 +65,16 @@ while RunSession
     if TaskParameters.GUI.Photometry
         Nidaq_photometry('WaitToStart');
     end
-
-    if TaskParameters.GUI.PharmacologyOn
-        prompt = {'Drug name:','Dosage:'};
-        dlgtitle = 'Pharmacology';
-        dims = [1 35];
-        definput = {'1x PBS','1 ml/kg i.p.'};
-        drugInfo = inputdlg(prompt,dlgtitle,dims,definput);
-        BpodSystem.Data.Custom.Pharmacology = drugInfo;
+    try
+        if TaskParameters.GUI.PharmacologyOn
+            prompt = {'Drug name:','Dosage:'};
+            dlgtitle = 'Pharmacology';
+            dims = [1 35];
+            definput = {'1x PBS','1 ml/kg i.p.'};
+            drugInfo = inputdlg(prompt,dlgtitle,dims,definput);
+            BpodSystem.Data.Custom.Pharmacology = drugInfo;
+        end
+    catch
     end
     
     %% run trial
